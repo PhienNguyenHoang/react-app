6d394e04d0484ea64128d04c423bf9c085bb2929
Phien <phiennh@dgroup.co>
2021-05-26 09:59:07 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+  const [count, setCount] = useState(1);
+  // const [color, setColor] = useState('');
+  const fetchCounter = () => 1;
+  const colors = ['red', 'yellow', 'green'];
+  useEffect(() => {
+    setCount(fetchCounter());
+  }, [])
+
+  // useEffect(() => {
+  //   setColor(colors[count % 3])
+  // }, [count])
+  // console.log("🚀 ~ file: App.js ~ line 5 ~ App ~ color", color)
+  const color = colors[count % 3];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', backgroundColor: `${color}` }}>
+      <div>Counter: {count}</div>
+      <button onClick={() => {setCount(count + 1)}}>
+        increase
+      </button>
     </div>
   );
 }
